029be7651320e07894a28f186a50bf2a46c1eb42
mingabire <mingabire@nhaschools.com>
2020-04-09 17:07:09 -0400

    remove ?

--- cypress/integration/helloWorld.spec.js
+++ cypress/integration/helloWorld.spec.js
@@ -6,6 +6,6 @@ describe('HelloWorld Test',()=>{
     cy.title().should('contain','Moise')
     cy.get('.btn').should('contain','HIRE ME')
     cy.get('body').should('contain','Moise Ingabire')
-    cy.get('.container > h1').should('contain','LOOKING FOR DEVELOPER?')
+    cy.get('.container > h1').should('contain','LOOKING FOR DEVELOPER')
    });
 })
